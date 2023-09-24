9d6216377cf15e7007d48d6fcbff888a15ad25d6
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-09-24 13:01:12 -0600

    Complete first three exercises

--- oop.rb
+++ oop.rb
@@ -3,7 +3,19 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+# class Unicorn
+#     def initialize(name)
+#         @name = name
+#         @color = "silver"
+#     end
 
+#     def say(string)
+#        "*~*#{string}*~*"
+#     end
+# end
+
+# connor = Unicorn.new("Connor")
+# p connor.say("I'm a magical being, welcome to the wonders of the universe")
 
 #  Write a class called Vampire
 #  it should have a dynamic name attribute
@@ -11,6 +23,22 @@
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
+# class Vampire
+#     def initialize(name, pet = "bat", thirsty = true)
+#         @name = name
+#         @pet = pet
+#         @thirsty = thirsty
+#     end
+
+#     def drink
+#         @thirsty = false
+#     end
+# end
+
+# thomas = Vampire.new("Thomas","snake")
+# p thomas
+# thomas.drink
+# p thomas
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -19,6 +47,33 @@
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
+class Dragon
+    attr_reader(:name, :rider, :color, :is_hungry)
+
+    def initialize(name, rider, color, is_hungry = true)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = is_hungry
+        @food = 0
+    end
+
+    def eat
+       @food = @food.next
+        if @food >= 4
+            @is_hungry = false
+        else
+        "Dragon is still hungry"
+        end
+    end
+end
+
+kyle = Dragon.new("Kyle","Thomas","black")
+kyle.eat
+kyle.eat
+p kyle.eat
+p kyle.is_hungry
+p kyle
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
